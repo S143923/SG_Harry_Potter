20e21999cafffb5f41171535fca4748dbc2fc6f4
Jan <janewaldable@gmail.com>
2018-11-24 17:54:22 +0100

    Initial website commit

---
+++ website/main.js
@@ -0,0 +1,32 @@
+// CAROUSEL
+$(document).ready(function(){
+    $('.wordcloud').carousel(
+        {
+            dist: 0,
+            padding: 0,
+            fullWidth: true,
+            indicators: true,
+            duration: 100,
+        }
+    );
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
+
+$(document).ready(function(){
+    $('#cloud').carousel({indicators: false, numVisible:7});
+});
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy({scrollOffset:1});
+});
+
+
+function selectBook(elem){
+    print("changed", elem);
+}
+
